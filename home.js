ffe7609010abeb12dea91d6c7f5135e4ca5d2d61
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-03 00:32:49 -0700

    error solved

--- home.js
+++ home.js
@@ -1,13 +1,18 @@
 import { onAuthStateChanged ,signOut } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { auth } from "./firebaseconfig.js";
 
-import { collection, addDoc ,  getDocs , doc, deleteDoc , updateDoc, } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"; 
+import { collection, addDoc ,  getDocs , doc, deleteDoc , Timestamp, updateDoc, query, where , orderBy, } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js"; 
 import { db } from "./config.js";
 
 const todoForm = document.querySelector('.form-todo')
 const todoInput = document.querySelector('.todo-input')
 const ul = document.querySelector('ul')
 const select = document.querySelector('#select')
+const reset = document.querySelector('.allCate')
+
+
+const cateBtn = document.querySelectorAll('.cateBtn')
+
 
 
 // global arr 
@@ -16,17 +21,48 @@ const select = document.querySelector('#select')
 let arr = []
 
 
+cateBtn.forEach((btn)=>{
+  arr = []
+  btn.addEventListener('click', async (e)=>{
+    const citiesRef = collection(db, "todos"); // shi ha
+    const q = query(citiesRef, where("categories", "==", e.target.innerHTML),
+    orderBy("time", "desc")
+  );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      arr.push({...doc.data() , id:doc.id})
+    });
+    rendersTodo()
+  })
+
+})
+
+
+reset.addEventListener("click", getData);
+
 // data get krne k lie 
 
 
+// async function getData() {
+//   arr = [];
+//   const q = query(collection(db, "todos"), orderBy("time", "desc"));
+//   const querySnapshot = await getDocs(q);
+//   querySnapshot.forEach((doc) => {
+//     arr.push({ ...doc.data(), id: doc.id });
+//   });
+//   rendersTodo();
+// }
 async function getData(){
-  const querySnapshot = await getDocs(collection(db, "todos"));
+  arr = []
+  const q =  query(collection(db, "todos"),orderBy("time", "desc"));
+  const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
   console.log(doc.data());
   arr.push({...doc.data() , id :doc.id})
 });
     rendersTodo()
 }
+
 getData()
 
 // screen pr data render krne ky liye 
